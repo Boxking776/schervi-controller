0161de18143421dd77d3c3c4887d052535bee47b
Boxking776 <64418882+Boxking776@users.noreply.github.com>
2022-12-25 13:28:02 +0000

    Update code.lua

--- code.lua
+++ code.lua
@@ -1,3 +1,4 @@
-for i = 1,3 do
-game.ReplicatedStorage.Purchase:InvokeServer("Jawbreaker")
-end
+local player = game.Players.LocalPlayer
+local TeleportService = game:GetService("TeleportService")
+local world = 5900179489
+TeleportService:Teleport(world, player)
