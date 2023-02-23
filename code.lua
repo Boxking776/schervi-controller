b522ce58df4aede4556e2421f501d306db621962
Boxking776 <64418882+Boxking776@users.noreply.github.com>
2023-02-23 20:12:49 +0000

    Update code.lua

--- code.lua
+++ code.lua
@@ -11,14 +11,14 @@ frame1.Draggable = false
 
 if asset_loading_function then
     local file_name = HttpService:GenerateGUID(false)
-    writefile(file_name..".gif", game:HttpGet("https://media.tenor.com/_tHxkI_ur48AAAAC/nuh-uh-nuh.gif"))
+    writefile(file_name..".png", game:HttpGet("https://cdn.discordapp.com/attachments/950485631262400562/1078409022698434590/20221007_115838.png"))
 
     local img = Instance.new("ImageLabel", frame1)
     img.Size = UDim2.new(1, 0, 1, 0)
     img.Active = true
-    img.Image = asset_loading_function(file_name..".gif")
+    img.Image = asset_loading_function(file_name..".png")
 
     task.wait(0.5)
 
-    delfile(file_name..".gif") 
+    delfile(file_name..".png") 
 end
